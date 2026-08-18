6647d04cd4ce13a186405bac5ca4e5742aa0be77
marwa-el-khanchoufi <elkhanmarwa2005@gmail.com>
2026-08-18 12:55:13 +0100

    assurer que provideHttpClient() est bien configuré pour que vos services puissent faire des requêtes HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http'; // <-- Importez ceci
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient() // <-- Ajoutez ceci pour activer HttpClient
   ]
 };
